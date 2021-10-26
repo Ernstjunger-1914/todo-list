f2da826a16359ef1fa3ca7aba9d380ff6f59d847
KGH <vot768@naver.com>
2021-10-26 10:57:55 +0900

    save

---
+++ README.md
@@ -0,0 +1,19 @@
+# todo-list
+react-nodejs-mongodb   
+
+# server
+> npm i express-generator -g   
+> express server
+
+# client
+> yarn create react-app client
+
+# server 측에 설치한 패키지
+- nodemon
+- body-parser
+- bcrypt
+- mongoose
+
+# client 측에 설치한 패키지
+- react-router-dom
+- axios
---
+++ server/app.js
@@ -0,0 +1,56 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyparser=require('body-parser');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const registerRouter = require('./routes/register');
+
+const app = express();
+const port=3333;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/register', registerRouter);
+
+mongoose.connect('mongodb+srv://kgh:0000@cluster-0.zgplc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=> {
+  console.log("MongoDB Connected..");
+}).catch(err=> {
+  console.log(err);
+});
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, ()=> {
+  console.log(`server running on port ${port}`);
+})
+
+module.exports = app;
