5d827d60047b57b0d1737964d2c6abf3cd9cea57
KGH <vot768@naver.com>
2021-10-27 17:09:20 +0900

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Landing from './component/Landing';
+import Login from './component/Login';
+import Register from './component/Register';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
