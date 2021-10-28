59325bd1c40a5e3faffaedea3fba36946c497175
KGH <vot768@naver.com>
2021-10-28 15:56:39 +0900

    액세스 차단됨

--- client/src/auth/Auth.js
+++ client/src/auth/Auth.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { auth } from "../action/UserAction";
 
 export default function(SpecificComponent, option, rootRoute=null) {
     function AuthenticationCheck(props) {
         const dispatch = useDispatch();
 
         useEffect(()=> {
-            dispatch(auth()).then(response => {
+            dispatch(auth).then(response => {
                 console.log(response);
 
                 if(!response.payload.isAuth) {
