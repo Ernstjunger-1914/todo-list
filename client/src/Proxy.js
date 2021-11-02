a411e37c4302f1aa3b80a6c96bc74c63e3a0a473
KGH <vot768@naver.com>
2021-11-02 15:25:38 +0900

    save

--- client/src/Proxy.js
+++ client/src/Proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports=function(app) {
-    app.use('/users', createProxyMiddleware({
+    app.use('/', createProxyMiddleware({
         target: 'http://localhost:3333',
         changeOrigin: true
     }));
