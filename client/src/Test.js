1000e3e40dafee76827c35f1c3e7375681c2680e
KGH <vot768@naver.com>
2021-11-05 13:52:28 +0900

    install faker

--- client/src/Test.js
+++ client/src/Test.js
@@ -1,12 +1,30 @@
-import { List } from 'react-virtualized';
+import { List, AutoSizer, CellMeasurer, CellMeasurerCache } from 'react-virtualized';
+import React, { useState } from 'react';
+import faker from 'faker';
 import './App.css';
 
-function Test() {
+export default function Test() {
+  const [people, setPeople]=React.useState([]);
+
+  React.useEffect(()=> {
+    setPeople([...Array(100).keys()].map(key=> {
+      return {
+        id: key,
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        bio: faker.lorem.lines(Math.random()*100),
+      };
+    }));
+  }, []);
+
   return (
-      <div className="App">
-          <List />
+      <div className="Test">
+        <ul>
+          {people.map((person)=> (
+            <li key={person.id}>
+              <h2>{person.name}</h2>
+            </li>
+          ))}
+        </ul>
       </div>
   );
 }
-
-export default Test;
